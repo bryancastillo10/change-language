10d04d44a1e8abb985007a3ad3f0ebafc3dac866
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2023-11-09 13:10:20 +0800

    add fun in settings.py

--- lang/views.py
+++ lang/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
+from django.utils.translation import get_language, activate, gettext
 
 
 def home(request):
-    trans = _("Hello")
+    trans = translate(language="zh")
     return render(request, "home.html", {"trans": trans})
+
+
+def translate(language):
+    cur_language = get_language()
+    try:
+        activate(language)
+        text = gettext("hello")
+    finally:
+        activate(cur_language)
+    return text
