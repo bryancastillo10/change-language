88f89a7ac345ee3d22bdc7916d369a221a956417
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-01-30 21:52:34 +0800

    Counter program done

--- 03_get_started/index.js
+++ 03_get_started/index.js
@@ -49,3 +49,30 @@ document.getElementById("hypoCalc").onclick = function () {
 
   document.getElementById("cLab").innerHTML = "Side C: " + c;
 };
+
+/// Counter Program
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = function () {
+  count++;
+  countLabel.textContent = count;
+};
+
+decreaseBtn.onclick = function () {
+  count--;
+  countLabel.textContent = count;
+};
+
+resetBtn.onclick = function () {
+  count = 0;
+  countLabel.textContent = count;
+};
+
+document.getElementById("nextBtn").onclick = function () {
+  window.location.href = "tutorial_part2.html";
+};
