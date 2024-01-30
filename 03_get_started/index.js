d61143d7983009c33eca375febe2825b09d66f74
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2024-01-30 16:46:18 +0800

    bro code tutorial

---
+++ 03_get_started/index.js
@@ -0,0 +1,51 @@
+//Try first
+
+function changeText() {
+  var firstButton = document.getElementById("change");
+
+  firstButton.innerHTML = "Let's get started! This text has changed. ";
+}
+
+function showAlert() {
+  window.alert("You are learning Javascript!");
+}
+
+function variableTest() {
+  let name = "Bro";
+  let age = 28;
+  let student = true;
+
+  document.getElementById("p1").innerHTML = "Hello " + name;
+  document.getElementById("p2").innerHTML = "You are " + age + " years old.";
+  document.getElementById("p3").innerHTML = "Enrolled: " + student;
+}
+
+function submitBtn() {
+  var textInput = document.getElementById("myText").value;
+
+  document.getElementById("reply").innerHTML = "You have typed: " + textInput;
+}
+
+function circumCalc() {
+  const PI = 3.14159;
+  let circum;
+
+  let radius = document.getElementById("inputRadius").value;
+  radius = Number(radius);
+
+  circum = 2 * PI * radius;
+  document.getElementById("answer").innerHTML =
+    "The circumference is " + circum;
+}
+
+document.getElementById("hypoCalc").onclick = function () {
+  var a = document.getElementById("A_text").value;
+  a = Number(a);
+
+  var b = document.getElementById("B_text").value;
+  b = Number(b);
+
+  var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+  document.getElementById("cLab").innerHTML = "Side C: " + c;
+};
