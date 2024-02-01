3d946f65d8e0daa7cf8bb0ff125cc5b1037428da
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2024-02-01 13:31:08 +0800

    random password generator

--- 03_get_started/part2.js
+++ 03_get_started/part2.js
@@ -41,13 +41,77 @@ mySubmit.onclick = function () {
   }
 };
 
-/// Variable Scope:
+/// Callback:
+sum(displayPage, 1, 2);
 
-functiion function1 (){
-    let x = 1;
-    console.log(x);
+function sum(callback, x, y) {
+  let result = x + y;
+  callback(result);
 }
 
-function redirectToIndex() {
-    window.location.href = "index.html";
+function displayConsole(result) {
+  console.log(result);
+}
+
+function displayPage(result) {
+  document.getElementById("myH4").textContent = result;
+}
+
+/// Random Password Generator
+function generatePassword(
+  length,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) {
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const symbolChars = "!@#$%^&*()_+-=";
+
+  let allowedChars = "";
+  let password = "";
+
+  allowedChars += includeLowercase ? lowercaseChars : "";
+  allowedChars += includeUppercase ? uppercaseChars : "";
+  allowedChars += includeNumbers ? numberChars : "";
+  allowedChars += includeSymbols ? symbolChars : "";
+
+  if (length <= 0) {
+    return "(Password length must be at least 1)";
   }
+  if (allowedChars.length === 0) {
+    return "(At least one set of characters needs to be selected)";
+  }
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars[randomIndex];
+  }
+
+  return password;
+}
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(
+  passwordLength,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+);
+
+// console.log(`Generated password: ${password}`);
+document.getElementById("myButton").onclick = function () {
+  document.getElementById("generated").textContent = password;
+};
+
+document.getElementById("prevBtn").onclick = function () {
+  window.location.href = "index.html";
+};
