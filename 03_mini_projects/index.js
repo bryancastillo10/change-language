53e591941731ec94e87546864715f0c2ce58dcf6
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-03-09 21:48:33 +0800

    delete old react

--- 03_mini_projects/index.js
+++ 03_mini_projects/index.js
@@ -102,3 +102,64 @@ function resetClock() {
   secondsElapsed = 0;
   setTime();
 }
+
+/* To Do List */
+let items = [];
+
+const itemsDiv = document.getElementById("items");
+const input = document.getElementById("itemInput");
+const storageKey = "items";
+
+function renderItems() {
+  itemsDiv.innerHTML = null;
+
+  for (const [idx, item] of Object.entries(items)) {
+    const contain = document.createElement("div");
+    contain.style.marginBottom = "10px";
+
+    const text = document.createElement("p");
+    text.style.display = "inline";
+    text.style.marginRight = "10px";
+    text.textContent = item;
+
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    button.onclick = () => removeItem(idx);
+
+    contain.appendChild(text);
+    contain.appendChild(button);
+
+    itemsDiv.appendChild(contain);
+  }
+}
+
+function loadItems() {
+  const oldItems = localStorage.getItem(storageKey);
+  if (oldItems) items = JSON.parse(oldItems);
+  renderItems();
+}
+
+function saveItems() {
+  const stringItems = JSON.stringify(items);
+  localStorage.setItem(storageKey, stringItems);
+}
+
+function addItem() {
+  const value = input.value;
+  if (!value) {
+    alert("You cannot add an empty item. Please try agaub,");
+    return;
+  }
+  items.push(value);
+  renderItems();
+  input.value = "";
+  saveItems();
+}
+
+function removeItem(idx) {
+  items.splice(idx, 1);
+  renderItems();
+  saveItems();
+}
+
+document.addEventListener("DOMContentLoaded", loadItems);
