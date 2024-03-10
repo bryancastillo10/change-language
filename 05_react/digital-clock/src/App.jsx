20c9b2ce0a19bf1d8d95cd4bb4443f407e1e2487
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-03-10 14:57:09 +0800

    digital clock and stop watch

---
+++ 05_react/digital-clock/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import DigitalClock from "./DigitalClock.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <DigitalClock />
+    </div>
+  );
+};
+
+export default App;
---
+++ 05_react/stopwatch/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import StopWatch from "./StopWatch.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <StopWatch />
+      <StopWatch />
+    </div>
+  );
+};
+
+export default App;
